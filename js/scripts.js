87d070366a34608a6257e7e5681dd9875425c3ae
gustavo-candido-silva <gustavo_gcs@outlook.com>
2024-10-07 20:58:36 -0300

    adding calculators and template generators

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,51 @@
-console.log("test")
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+const multiplicationTitleSpan= document.querySelector("#multiplication-title span");
+const multiplicationTable = document.querySelector("#multiplication-operations");
+
+const changeTitle = (number) => {
+    multiplicationTitleSpan.innerHTML = number
+}
+
+const checkEmpty = (number, multiplicator) => {
+    if(!number||!multiplicator)
+        return true
+    return false
+}
+
+const createTable = (number, multiplicator) => {
+    const parser = new DOMParser();
+    multiplicationTable.innerHTML = "";
+
+    for (let multiplier = 0; multiplier <= multiplicator; multiplier++) {
+        const result = number * multiplier;
+
+        const template = 
+            `<div class="row">
+                <div class="operation">${number} x ${multiplier} = </div>
+                <div class="result">${result}</div>
+            </div>`;
+
+        const htmlTemplate = parser.parseFromString(template, "text/html");
+
+        const row = htmlTemplate.querySelector(".row");
+
+        changeTitle(number);
+        multiplicationTable.appendChild(row);
+
+    }
+
+}
+
+multiplicationForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const number = +numberInput.value;
+    const multiplicator = +multiplicationInput.value;
+
+    if(checkEmpty(number, multiplicator)) return;
+
+    createTable(number, multiplicator);
+})
+
